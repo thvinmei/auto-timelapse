f80c281569a79ae7d61f4023f3e7b543d362b884
Toshiaki Hashimoto <thvinmei+git@gmail.com>
2019-05-06 00:50:35 +0900

    事前に拡張子JPGをjpgに変換することで複数回目の実行時にエラーが発生するのを回避するようにした

--- timelapse.sh
+++ timelapse.sh
@@ -10,16 +10,22 @@ FPS=$2
 
 ## rename files in target folder
 cd ../$TargetDir
-mkdir ../tempdir
+mkdir -p ../tempdir
+mkdir -p ../export
 
 i=0
-for n in *.JPG
+
+rename 's/JPG/jpg/' *.JPG
+rename 's/JPG/jpg/' *.JPG.xmp
+
+for n in *.jpg
 do
     ##-------------------------------
     ## jpgファイルをリネームして保存
     ##-------------------------------
     mv $n $(printf %05d $i).jpg
-#    mv $n.xmp $(printf %05d $i).jpg.xmp
+    # darktableの編集ファイルもリネーム
+    mv $n.xmp $(printf %05d $i).jpg.xmp
 
     j=$i
     i=$(expr $i + 1)
@@ -28,12 +34,17 @@ do
     ## 比較明合成
     ##-------------------------------
     if [ $j -eq 0 ] ; then
-        echo "init temp"
         cp $(printf %05d $j).jpg ../tempdir/$(printf %05d $j).jpg
     fi
 
-    echo "composite:" "img"$j "+" "temp"$j "=" "temp"$i
     composite -compose lighten $(printf %05d $j).jpg ../tempdir/$(printf %05d $j).jpg ../tempdir/$(printf %05d $i).jpg
 done
+##-------------------------------
+## 出力動画・画像の生成
+##-------------------------------
+yes|ffmpeg -r $FPS -i "%05d.jpg" -r $FPS -vcodec libx264 -crf 0 -g $FPS ../export/${TargetDir}.mp4
+yes|ffmpeg -r $FPS -i "../tempdir/%05d.jpg" -r $FPS -vcodec libx264 -crf 0 -g $FPS ../export/${TargetDir}-composited.mp4
+cp ../tempdir/$(printf %05d $i).jpg ../export/${TargetDir}-composited.jpg
 
-avconv -r $FPS -i "%05d.jpg" -r $FPS -vcodec libx264 -crf 0 -g $FPS ../export/tl-${TargetDir}.mp4
+# お片付け
+rm -rf ../tempdir
