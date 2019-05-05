26663d5383d75acad80fbc7e7ca609fbbe658429
Toshiaki Hashimoto <thvinmei+git@gmail.com>
2019-05-06 00:17:16 +0900

    比較明合成した画像を連続生成するようにした

--- timelapse.sh
+++ timelapse.sh
@@ -9,12 +9,31 @@ TargetDir=$1
 FPS=$2
 
 ## rename files in target folder
-cd $TargetDir
+cd ../$TargetDir
+mkdir ../tempdir
+
 i=0
 for n in *.JPG
 do
+    ##-------------------------------
+    ## jpgファイルをリネームして保存
+    ##-------------------------------
     mv $n $(printf %05d $i).jpg
+#    mv $n.xmp $(printf %05d $i).jpg.xmp
+
+    j=$i
     i=$(expr $i + 1)
+
+    ##-------------------------------
+    ## 比較明合成
+    ##-------------------------------
+    if [ $j -eq 0 ] ; then
+        echo "init temp"
+        cp $(printf %05d $j).jpg ../tempdir/$(printf %05d $j).jpg
+    fi
+
+    echo "composite:" "img"$j "+" "temp"$j "=" "temp"$i
+    composite -compose lighten $(printf %05d $j).jpg ../tempdir/$(printf %05d $j).jpg ../tempdir/$(printf %05d $i).jpg
 done
 
 avconv -r $FPS -i "%05d.jpg" -r $FPS -vcodec libx264 -crf 0 -g $FPS ../export/tl-${TargetDir}.mp4
